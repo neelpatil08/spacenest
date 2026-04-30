01ee109e96e4b71264cca5a66709ec64346e2ad6
neelpatil08 <neel86834@gmail.com>
2026-05-01 00:31:22 +0530

    added api test route

--- spacenest-your-flexible-office-main/backend/server.js
+++ spacenest-your-flexible-office-main/backend/server.js
@@ -9,6 +9,9 @@ const app = express();
 // ===== Middleware =====
 app.use(cors());
 app.use(express.json());
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working properly 🚀" });
+});
 
 // ===== Schema =====
 const userSchema = new mongoose.Schema({
